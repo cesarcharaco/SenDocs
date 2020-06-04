24a68854142731f15f5ce58f85a6c4265c8b4860
example <example@example.com>
2020-06-04 10:27:25 -0400

    animando editar archivo

--- client/src/pages/Documento/Edit.vue
+++ client/src/pages/Documento/Edit.vue
@@ -1,114 +1,87 @@
 <template>
   <div>
+    <animation-transition :animation-in-type="AnimationType.BOUNCEINUP" :animation-out-type="AnimationType.ROLLOUT">
+      <q-item-label header class="text-bold" v-show="show">Edita tu Archivo</q-item-label>
+    </animation-transition>
     <q-card
-      class="full-width shadow-3"
+      class="full-width shadow-3 q-mt-sm"
       bordered
     >
       <q-card-section>
         <div class="row justify-center">
-          <div class="column col">
-            <div class="col">
-              <q-input
-                dense
-                v-model="form.etiqueta"
-                label="Standard"
-              />
+          <animation-transition :animation-in-type="AnimationType.BOUNCEINLEFT" :animation-out-type="AnimationType.ROLLOUT">
+            <div class="column col" v-show="show">
+              <div class="col">
+                <q-input
+                  dense
+                  v-model="form.etiqueta"
+                  label="Etiqueta"
+                />
+              </div>
+              <div class="col text-bold">
+                <q-input
+                  dense
+                  v-model="form.nombre"
+                  label="Nombre"
+                />
+              </div>
+              <div class="col text-bold">
+                <q-input
+                  dense
+                  autogrow
+                  v-model="form.descripcion"
+                  label="Descripcion"
+                />
+              </div>
             </div>
-            <div class="col text-bold">
-              <q-input
-                dense
-                v-model="form.nombre"
-                label="Standard"
-              />
+          </animation-transition>
+          <animation-transition :animation-in-type="AnimationType.BOUNCEINDOWN" :animation-out-type="AnimationType.ROLLOUT">
+            <q-separator
+              vertical
+              inset
+              class="q-ma-sm"
+              v-show="show"
+            />
+          </animation-transition>
+          <animation-transition :animation-in-type="AnimationType.BOUNCEINRIGHT" :animation-out-type="AnimationType.ROLLOUT">
+            <div class="q-mt-sm column col" v-show="show">
+              <q-carousel
+                animated
+                v-model="slide"
+                arrows
+                infinite
+                style="width:100%; height:100px"
+              >
+                <q-carousel-slide
+                  v-for="(arc, index) in form.archivos"
+                  :key="index"
+                  :name="index"
+                  :img-src="arc.url"
+                  style="width:100%"
+                />
+              </q-carousel>
             </div>
-            <div class="col text-bold">
-              <q-input
-                dense
-                autogrow
-                v-model="form.descripcion"
-                label="Standard"
-              />
-            </div>
-          </div>
-          <q-separator
-            vertical
-            inset
-            class="q-ma-sm"
-          />
-          <div class="q-mt-sm column col">
-            <q-carousel
-              animated
-              v-model="slide"
-              arrows
-              infinite
-              style="width:100%; height:100px"
-            >
-              <q-carousel-slide
-                v-for="(arc, index) in form.archivos"
-                :key="index"
-                :name="index"
-                :img-src="arc.url"
-                style="width:100%"
-              />
-            </q-carousel>
-          </div>
+          </animation-transition>
         </div>
         <div class="row justify-center q-mt-md">
-          <div class="column col">
-            <div class="text-bold">
-              <div>Fecha de Vencimiento:</div>
-              <div class="text-h6"> {{form.fvencimiento}} </div>
-            </div>
-            <div>
-              <q-btn
-                icon="event"
-                round
-                color="primary"
-              >
-                <q-popup-proxy
-                  @before-show="fvencimiento = form.fvencimiento"
-                  transition-show="scale"
-                  transition-hide="scale"
-                >
-                  <q-date v-model="fvencimiento">
-                    <div class="row items-center justify-end q-gutter-sm">
-                      <q-btn
-                        label="Cancel"
-                        color="primary"
-                        flat
-                        v-close-popup
-                      />
-                      <q-btn
-                        label="OK"
-                        color="primary"
-                        flat
-                        @click="form.fvencimiento = fvencimiento"
-                        v-close-popup
-                      />
-                    </div>
-                  </q-date>
-                </q-popup-proxy>
-              </q-btn>
-            </div>
-          </div>
-          <div>
-            <div class="column col">
+          <animation-transition :animation-in-type="AnimationType.BOUNCEINLEFT" :animation-out-type="AnimationType.ROLLOUT">
+            <div class="column col" v-show="show">
               <div class="text-bold">
-                <div>Hora de Vencimiento:</div>
-                <div class="text-h6"> {{form.hvencimiento}} </div>
+                <div>Fecha de Vencimiento:</div>
+                <div class="text-h6"> {{form.fvencimiento}} </div>
               </div>
-              <div class="justify-center">
+              <div>
                 <q-btn
-                  icon="access_time"
+                  icon="event"
                   round
                   color="primary"
                 >
                   <q-popup-proxy
-                    @before-show="hvencimiento = form.hvencimiento"
+                    @before-show="fvencimiento = form.fvencimiento"
                     transition-show="scale"
                     transition-hide="scale"
                   >
-                    <q-time v-model="hvencimiento">
+                    <q-date v-model="fvencimiento">
                       <div class="row items-center justify-end q-gutter-sm">
                         <q-btn
                           label="Cancel"
@@ -120,15 +93,56 @@
                           label="OK"
                           color="primary"
                           flat
-                          @click="form.hvencimiento = hvencimiento"
+                          @click="form.fvencimiento = fvencimiento"
                           v-close-popup
                         />
                       </div>
-                    </q-time>
+                    </q-date>
                   </q-popup-proxy>
                 </q-btn>
               </div>
             </div>
+          </animation-transition>
+          <div>
+            <animation-transition :animation-in-type="AnimationType.BOUNCEINRIGHT" :animation-out-type="AnimationType.ROLLOUT" v-show="show">
+              <div class="column col">
+                <div class="text-bold">
+                  <div>Hora de Vencimiento:</div>
+                  <div class="text-h6"> {{form.hvencimiento}} </div>
+                </div>
+                <div class="justify-center">
+                  <q-btn
+                    icon="access_time"
+                    round
+                    color="primary"
+                  >
+                    <q-popup-proxy
+                      @before-show="hvencimiento = form.hvencimiento"
+                      transition-show="scale"
+                      transition-hide="scale"
+                    >
+                      <q-time v-model="hvencimiento">
+                        <div class="row items-center justify-end q-gutter-sm">
+                          <q-btn
+                            label="Cancel"
+                            color="primary"
+                            flat
+                            v-close-popup
+                          />
+                          <q-btn
+                            label="OK"
+                            color="primary"
+                            flat
+                            @click="form.hvencimiento = hvencimiento"
+                            v-close-popup
+                          />
+                        </div>
+                      </q-time>
+                    </q-popup-proxy>
+                  </q-btn>
+                </div>
+              </div>
+            </animation-transition>
           </div>
         </div>
       </q-card-section>
@@ -136,26 +150,33 @@
       <q-separator />
 
       <q-card-actions align="right">
-        <q-btn
-          class="glossy q-pa-sm"
-          rounded
-          color="primary"
-          label="Guardar"
-          style="width:100px"
-        />
+        <animation-transition :animation-in-type="AnimationType.BOUNCEINLEFT" :animation-out-type="AnimationType.ROLLOUT">
+          <q-btn
+            class="glossy q-pa-sm"
+            rounded
+            color="primary"
+            label="Guardar"
+            style="width:100px"
+            v-show="show"
+          />
+        </animation-transition>
       </q-card-actions>
-
     </q-card>
-
   </div>
 </template>
 
 <script>
+import {AnimationVueTransition, AnimationVueTransitionType} from 'vue-animation'
 export default {
+  components: {
+    [AnimationVueTransition.name]: AnimationVueTransition,
+  },
   data () {
     return {
       hvencimiento: '',
       fvencimiento: '',
+      AnimationType: AnimationVueTransitionType,
+      show: false,
       form: {
         etiqueta: 'Etiqueta',
         nombre: 'Nombre',
@@ -175,7 +196,10 @@ export default {
       },
       slide: 1
     }
-  }
+  },
+  mounted () {
+    this.show = true
+  },
 }
 </script>
 
